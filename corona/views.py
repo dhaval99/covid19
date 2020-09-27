a46bfd8a11506d83d6fc33345d2494c306a65808
Dhaval Shah <dhaval99@yorku.ca>
2020-09-27 16:35:35 -0400

    version fixes

---
+++ corona/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from data import kvpairs
+from data import orig_pairs
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def info(request):
+    country = request.POST['cName'].lower().strip()
+    send = " "
+    if country == "usa" or country == "united states of america":
+        country = "us"
+    elif country == "uk":
+        country = "united kingdom"
+    elif country == "uae":
+        country = "united arab emirates"
+    elif country == "south korea":
+        country = "korea, south"
+    for i in kvpairs:
+        if i == country:
+            send = kvpairs[i]
+    return render(request, "jinja.html", {'number': send})
---
+++ data.py
@@ -0,0 +1,56 @@
+import os
+from bs4 import BeautifulSoup
+# from selenium.webdriver import Chrome
+from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+import time
+from covid.settings import DEBUG
+from selenium import webdriver
+
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+
+url = "https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
+
+# GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
+# CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# if DEBUG:
+
+#     chrome_options.binary_location = GOOGLE_CHROME_BIN
+
+# if DEBUG:
+
+#     with Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options) as browser:
+#         browser.get(url)
+#         time.sleep(2)
+#         html = browser.page_source
+# else:
+
+#     with Chrome(options=chrome_options) as browser:
+#         browser.get(url)
+#         time.sleep(2)
+#         html = browser.page_source
+
+browser.get(url)
+time.sleep(2)
+html = browser.page_source
+
+html = BeautifulSoup(html,'html.parser')
+
+result = html.find("margin-container", {"class": "left right top"})
+kvpairs = {}
+orig_pairs = {}
+for tag in result.find_all("h5"):
+    dataGroup = tag.contents
+    num = dataGroup[0].get_text()
+    if dataGroup[2].get_text() == "US":
+        org_country = "United States Of America"
+    elif dataGroup[2].get_text() == "Korea, South":
+        org_country = "South Korea"
+    else:
+        org_country = dataGroup[2].get_text()
+    country = dataGroup[2].get_text().lower()
+    kvpairs[country] = num
+    orig_pairs[org_country] = num
